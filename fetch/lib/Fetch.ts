d8a5decef54eb5e8e3b4bda2206a0595c23ee4dc
Oliver Caine <olliecaine@gmail.com>
2019-04-05 00:19:22 +1100

    Adding link to better examples

--- fetch/lib/Fetch.ts
+++ fetch/lib/Fetch.ts
@@ -1,3 +1,5 @@
+// Better : https://www.carlrippon.com/fetch-with-async-await-and-typescript/
+
 export async function fetchAsync(url: string) {
     const data = await (await (fetch(url)
         .catch((error) => {
