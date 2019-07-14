bf8657ba1fd5fd6170958fa163952ca3b074409d
Oliver Caine <olliecaine@gmail.com>
2019-07-15 00:58:11 +1000

    Deleting unused files

--- emailer/lib/add.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { add } from '../lib';
-
-describe('# add', () => {
-    it('should add two numbers', () => {
-        expect(add(3, 5)).to.equal(8);
-    });
-});
