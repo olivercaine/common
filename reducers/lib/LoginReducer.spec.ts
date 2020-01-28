0292bf7a64ebaecd34e8132aebdbd63d91e347ef
Oliver Caine <olliecaine@gmail.com>
2020-01-29 01:08:26 +1100

    Adding provide email reducer

--- reducers/lib/LoginReducer.spec.ts
+++ reducers/lib/LoginReducer.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { createAction } from './ActionCreator';
-import { DID_LOGIN_SUCCESSFULLY, ILoginState, IUser, loginReducer } from './LoginReducer';
+import { DID_LOGIN_SUCCESSFULLY, DID_PROVIDE_EMAIL, ILoginState, IUser, loginReducer } from './LoginReducer';
 
 describe('Given no user exists', () => {
     describe('When successfully logging in', () => {
@@ -17,6 +17,24 @@ describe('Given no user exists', () => {
                 profilePicture: 'me.jpg',
             };
             const action = createAction(DID_LOGIN_SUCCESSFULLY, payload);
+
+            // Act
+            const newState = loginReducer(state, action);
+
+            // Assert
+            expect(newState.user!.email).to.equal('me@mail.com');
+        });
+    });
+    describe('When providing email address', () => {
+        it('Then email address should exist on state', () => {
+            // Arrange
+            const state: ILoginState = {
+                error: undefined,
+                isLoading: false,
+                user: undefined,
+            };
+            const payload: string = 'me@mail.com';
+            const action = createAction(DID_PROVIDE_EMAIL, payload);
 
             // Act
             const newState = loginReducer(state, action);
