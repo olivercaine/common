3c0cb4c6e2e8e22804423c60e6a3c97b2214762f
Oliver Caine <olliecaine@gmail.com>
2020-04-14 00:35:23 +1000

    Shorthand using momentjs

--- utils/lib/Date.ts
+++ utils/lib/Date.ts
@@ -5,17 +5,5 @@ export const prettyDate = (date: Date, format: string = 'dddd, Do MMMM [at] h:mm
 }
 
 export const fullYearsBetweenDates = (fromDate: Date, toDate?: Date): number => {
-  
-    if (toDate === undefined) {
-      toDate = new Date();
-    }
-
-    let age = toDate.getFullYear() - fromDate.getFullYear();
-
-    const months = toDate.getMonth() - fromDate.getMonth();
-    if (months < 0 || months === 0 && toDate.getDate() < fromDate.getDate()) {
-      age--;
-    }
-
-    return age;
+  return moment(toDate || new Date()).diff(moment(fromDate), 'years');
 }
