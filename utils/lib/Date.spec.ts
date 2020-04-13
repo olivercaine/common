e78a3b2a08430755d67f9d9f7431fd214f3cf9b4
Oliver Caine <olliecaine@gmail.com>
2020-04-14 00:34:53 +1000

    Calculating years between dates

--- utils/lib/Date.spec.ts
+++ utils/lib/Date.spec.ts
@@ -1,14 +1,41 @@
 import { expect } from 'chai';
-import { prettyDate } from './Date'
+import { fullYearsBetweenDates, prettyDate } from './Date';
 
 describe('Given a date', () => {
   describe('When prettyfying it', () => {
     it('Then it should be formatted correctly', () => {
-      const date = new Date(2017, 0, 21, 19)
+      const date = new Date(2017, 0, 21, 19);
 
-      const result = prettyDate(date)
+      const result = prettyDate(date);
 
-      expect(result).to.equal('Saturday, 21st January at 7:00pm')
-    })
-  })
-})
+      expect(result).to.equal('Saturday, 21st January at 7:00pm');
+    });
+  });
+});
+
+describe.only('When calculating number of years between dates', () => {
+  describe('Given the dates are just under a year apart', () => {
+    it('Then the number of years should be 0', () => {
+      expect(fullYearsBetweenDates(new Date(2000, 1, 10), new Date(2001, 1, 9))).to.equal(0);
+    });
+  });
+  describe('Given the dates are exactly a year apart', () => {
+    it('Then the number of years should be 1', () => {
+      expect(fullYearsBetweenDates(new Date(2000, 1, 10), new Date(2001, 1, 10))).to.equal(1);
+    });
+  });
+  describe('Given the dates are just over a year apart', () => {
+    it('Then the number of years should be 1', () => {
+      expect(fullYearsBetweenDates(new Date(2000, 1, 10), new Date(2001, 1, 11))).to.equal(1);
+    });
+  });
+  describe('Given only one date provided which 10 years ago', () => {
+    it('Then the number of years should be 10', () => {
+      const date10YearsAgo = new Date(new Date().getFullYear() - 10, new Date().getMonth(), new Date().getDate());
+
+      const result = fullYearsBetweenDates(date10YearsAgo);
+
+      expect(result).to.equal(10);
+    });
+  });
+});
--- utils/lib/Date.ts
+++ utils/lib/Date.ts
@@ -1,5 +1,21 @@
-import * as moment from 'moment'
+import * as moment from 'moment';
 
 export const prettyDate = (date: Date, format: string = 'dddd, Do MMMM [at] h:mma'): string => {
     return moment(date).format(format); // https://momentjs.com/
+}
+
+export const fullYearsBetweenDates = (fromDate: Date, toDate?: Date): number => {
+  
+    if (toDate === undefined) {
+      toDate = new Date();
+    }
+
+    let age = toDate.getFullYear() - fromDate.getFullYear();
+
+    const months = toDate.getMonth() - fromDate.getMonth();
+    if (months < 0 || months === 0 && toDate.getDate() < fromDate.getDate()) {
+      age--;
+    }
+
+    return age;
 }
