9b93facb66eb1d56e87a59042d1d00ba0e2f5605
Oliver Caine <olliecaine@gmail.com>
2020-07-29 01:25:30 +1000

    Adding generic http requests - GET, POST, PUT & DELETE using cross-fetch

--- fetch/lib/Fetch.ts
+++ fetch/lib/Fetch.ts
@@ -1,14 +1,50 @@
-// Better : https://www.carlrippon.com/fetch-with-async-await-and-typescript/
+import { fetch, Request } from 'cross-fetch';
 
-export async function fetchAsync(url: string) {
-    const data = await (await (fetch(url)
-        .catch((error) => {
-            throw new Error(error);
-        })
-    ));
-    return data;
+// Based on https://www.carlrippon.com/fetch-with-async-await-and-typescript/
+
+async function http<T>(request: RequestInfo): Promise<IHttpResponse<T>> {
+    const response: IHttpResponse<T> = await fetch(request);
+    try { // May error if there's no body
+        response.parsedBody = await response.json();
+    } catch (ex) {
+        throw new Error('No body in response');
+    }
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    return response;
 }
 
-export async function fetchAsyncFake(callback, delay: number = 350) {
-    setTimeout(callback, delay);
+export interface IHttpResponse<T> extends Response { parsedBody?: T; }
+
+// CRUD
+export async function get<T>(
+    path: string,
+    args: RequestInit = { method: 'get' },
+): Promise<IHttpResponse<T>> {
+    return http<T>(new Request(path, args));
+}
+
+export async function post<T>(
+    path: string,
+    body: any,
+    args: RequestInit = { method: 'post', body: JSON.stringify(body) },
+): Promise<IHttpResponse<T>> {
+    return http<T>(new Request(path, args));
+}
+
+export async function put<T>(
+    path: string,
+    body: any,
+    args: RequestInit = { method: 'put', body: JSON.stringify(body) },
+): Promise<IHttpResponse<T>> {
+    return http<T>(new Request(path, args));
+}
+
+export async function del<T>(
+    path: string,
+    body: any,
+    args: RequestInit = { method: 'delete', body: JSON.stringify(body) },
+): Promise<IHttpResponse<T>> {
+    return http<T>(new Request(path, args));
 }
