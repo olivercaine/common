d6c526a8e746bc179e3c682fa67068c3b3a97bb6
Oliver Caine <olliecaine@gmail.com>
2020-08-02 19:48:40 +1000

    Adding test

--- fetch/lib/Fetch.spec.ts
+++ fetch/lib/Fetch.spec.ts
@@ -1,9 +1,17 @@
 import { expect } from 'chai';
 
-describe('Putting this here', () => {
-    describe('So the tests pass', () => {
-        it('And the build completes', () => {
-            expect(true).to.equal(true);
+describe('Given two headers', () => {
+    describe('When merging them together', () => {
+        it('Should become one object', () => {
+            // Arrange
+            const headers = { Authorization: 'Basic 1234' }
+            const additionalHeaders = { 'Content-Type': 'application/json' }
+
+            // Act
+            const merged = { ...headers, ...additionalHeaders };
+
+            // Assert
+            expect(JSON.stringify(merged)).to.equal(JSON.stringify({ 'Authorization': 'Basic 1234', 'Content-Type': 'application/json' }));
         });
     });
 });
